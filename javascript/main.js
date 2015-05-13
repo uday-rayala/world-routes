466f92ececfb377389b36ef6d8c059b40c97b578
Udayakumar Rayala <uday.rayala@gmail.com>
2015-05-13 14:35:25 +0530

    Transitioning routes

--- javascript/main.js
+++ javascript/main.js
@@ -22,8 +22,7 @@ var mapGroup = svg.append("g").attr("class", "map");
 var citiesGroup = svg.append("g").attr("class", "cities");
 var routesGroup = svg.append("g").attr("class", "routes");
 
-d3.json("/json/world-50m.json", function(error,
-                                         world) {
+d3.json("/json/world-50m.json", function(error, world) {
     mapGroup.selectAll("path")
         .data(topojson.feature(world, world.objects.countries).features)
         .enter()
@@ -35,29 +34,42 @@ d3.json("/json/world-50m.json", function(error,
 
     var bangalore = [77.5945627, 12.9715987];
     var newYork = [-74.0059413, 40.7127837];
-    var cities = [bangalore, newYork];
-    var routes = [{source: bangalore, destination: newYork}];
+    var chennai = [80.2252278, 13.0597049];
+    var delhi = [77.13, 28.38];
+
+    var cities = [bangalore, chennai, newYork, delhi];
+    var routes = [{source: bangalore, destination: newYork},
+                  {source: newYork, destination: chennai},
+                  {source: bangalore, destination: chennai},
+                  {source: chennai, destination: delhi}];
 
     citiesGroup.selectAll("circle")
         .data(cities)
         .enter()
         .append("circle")
-        .attr("cx", function(city) {
-            return projection(city)[0];
-        })
+        .attr("cx", function(city) { return projection(city)[0]; })
         .attr("cy", function(city) { return projection(city)[1]; })
-        .attr("r", 5)
+        .attr("r", 3)
         .attr("fill", "red");
 
 
-    routesGroup.selectAll("path")
+    var newRoutes = routesGroup.selectAll("path")
         .data(routes)
         .enter()
         .append("path")
         .attr("class", "route")
         .style("fill", "none")
-        .style("stroke", "red")
-        .attr("d", function(route) {
-            return path({type: "LineString", coordinates: [route.source, route.destination]});
+        .style("stroke", "red");
+
+    newRoutes
+        .transition()
+        .duration(1000)
+        .ease("linear")
+        .attrTween("d", function(route) {
+            var interpolateFn = d3.geo.interpolate(route.source, route.destination);
+
+            return function(t) {
+                return path({type: "LineString", coordinates: [route.source, interpolateFn(t)]});
+            };
         });
 });
