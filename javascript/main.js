bbfa4fd7264527eec7bddeb9fdad5864f9eac3b0
Udayakumar Rayala <uday.rayala@gmail.com>
2015-05-13 19:00:49 +0530

    Added cities as dropdown

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,5 @@
-var width = 960,
-    height = 960;
+var width = window.innerWidth,
+    height = window.innerWidth;
 
 var projection = d3.geo.mercator()
     .scale((width + 1) / 2 / Math.PI)
@@ -22,7 +22,23 @@ var mapGroup = svg.append("g").attr("class", "map");
 var citiesGroup = svg.append("g").attr("class", "cities");
 var routesGroup = svg.append("g").attr("class", "routes");
 
-d3.json("json/world-50m.json", function(error, world) {
+var world, cities, routes;
+
+var findCoordinates = function(code) {
+    var airport = cities.filter(function(city) {
+        return city.iata === code || city.airport === code;
+    })[0];
+
+    if(airport === undefined) {
+        console.error("Unable to find airport:", code);
+    }
+
+    return [airport.longitude, airport.latitude];
+};
+
+var draw = function() {
+    if(!world) return;
+
     mapGroup.selectAll("path")
         .data(topojson.feature(world, world.objects.countries).features)
         .enter()
@@ -32,44 +48,71 @@ d3.json("json/world-50m.json", function(error, world) {
         .style("fill", "white");
 
 
-    var bangalore = [77.5945627, 12.9715987];
-    var newYork = [-74.0059413, 40.7127837];
-    var chennai = [80.2252278, 13.0597049];
-    var delhi = [77.13, 28.38];
-
-    var cities = [bangalore, chennai, newYork, delhi];
-    var routes = [{source: bangalore, destination: newYork},
-                  {source: newYork, destination: chennai},
-                  {source: bangalore, destination: chennai},
-                  {source: chennai, destination: delhi}];
-
     citiesGroup.selectAll("circle")
         .data(cities)
         .enter()
         .append("circle")
-        .attr("cx", function(city) { return projection(city)[0]; })
-        .attr("cy", function(city) { return projection(city)[1]; })
-        .attr("r", 3)
+        .attr("cx", function(city) {
+            return projection([city.longitude, city.latitude])[0];
+        })
+        .attr("cy", function(city) {
+            return projection([city.longitude, city.latitude])[1];
+        })
+        .attr("r", 1)
         .attr("fill", "red");
+};
 
+d3.json("json/world-50m.json", function(error, data) {
+    world = data;
+    draw();
+});
 
-    var newRoutes = routesGroup.selectAll("path")
-        .data(routes)
+d3.csv("json/airports.csv", function(error, data) {
+    cities = data;
+
+    d3.select("select.airports")
+        .selectAll("option")
+        .data(data)
         .enter()
-        .append("path")
-        .attr("class", "route")
-        .style("fill", "none")
-        .style("stroke", "red");
+        .append("option")
+        .attr("value", function(d) { return d.iata; })
+        .text(function(d) { return d.airport; });
 
-    newRoutes
-        .transition()
-        .duration(1000)
-        .ease("linear")
-        .attrTween("d", function(route) {
-            var interpolateFn = d3.geo.interpolate(route.source, route.destination);
-
-            return function(t) {
-                return path({type: "LineString", coordinates: [route.source, interpolateFn(t)]});
-            };
+    d3.select("select.airports").on("change", function(d) {
+        var selectedAirport = this.options[this.selectedIndex].value;
+        var routesToRender = routes.filter(function(route) {
+            return route.origin === selectedAirport;
         });
+
+        routesGroup.selectAll("path").remove();
+
+        var newRoutes = routesGroup.selectAll("path")
+            .data(routesToRender)
+            .enter()
+            .append("path")
+            .attr("class", "route")
+            .style("fill", "none")
+            .style("stroke", "red");
+
+        newRoutes
+            .transition()
+            .duration(1000)
+            .ease("linear")
+            .attrTween("d", function(route) {
+                var origin = findCoordinates(route.origin);
+                var destination = findCoordinates(route.destination);
+                var interpolateFn = d3.geo.interpolate(origin, destination);
+
+                return function(t) {
+                    return path({type: "LineString", coordinates: [origin, interpolateFn(t)]});
+                };
+            });
+    })
+
+    routes = [];
+    draw();
+});
+
+d3.json("json/routes.json", function(error, data) {
+    routes = data;
 });
